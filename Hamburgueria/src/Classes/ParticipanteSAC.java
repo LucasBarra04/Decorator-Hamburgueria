1b6064ea6a3deeca24f26ebe96799039fbffbaee
Lucas Barra Machado <lucasbm.jf@gmail.com>
2026-05-28 20:08:07 -0300

    Mediator (SAC)

---
+++ Hamburgueria/src/Classes/ParticipanteSAC.java
@@ -0,0 +1,7 @@
+package Classes;
+public abstract class ParticipanteSAC {
+    protected SAC sac;
+    public ParticipanteSAC(SAC sac) { this.sac = sac; }
+    public abstract String getNome();
+    public abstract void receberMensagem(String mensagem, String remetente);
+}
