5e0ef1b86f43bd38e05d8f2de41ce4a42120d893
Lucas Barra Machado <lucasbm.jf@gmail.com>
2026-06-13 10:54:21 -0300

    Proxy - Controle de Acesso aos Dados do Pedido

---
+++ Hamburgueria/src/Classes/DadosPedido.java
@@ -0,0 +1,26 @@
+package Classes;
+
+public class DadosPedido implements IDadosPedido {
+    private PedidoCompleto pedido;
+
+    public DadosPedido(PedidoCompleto pedido) {
+        this.pedido = pedido;
+        System.out.println("DadosPedido: carregando dados sensíveis do pedido...");
+    }
+
+    @Override
+    public String getNomeCliente()    { return pedido.getNomeCliente(); }
+
+    @Override
+    public String getFormaPagamento() { return pedido.getFormaPagamento(); }
+
+    @Override
+    public double getCustoTotal()     { return pedido.getCustoTotal(); }
+
+    @Override
+    public String getResumo() {
+        return "Cliente: " + getNomeCliente()
+             + " | Pagamento: " + getFormaPagamento()
+             + " | Total: R$" + getCustoTotal();
+    }
+}
---
+++ Hamburgueria/src/Classes/IDadosPedido.java
@@ -0,0 +1,8 @@
+package Classes;
+
+public interface IDadosPedido {
+    String getNomeCliente();
+    String getFormaPagamento();
+    double getCustoTotal();
+    String getResumo();
+}
